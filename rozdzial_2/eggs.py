99fa9695f727d48ed2d7886d41f72b2a784210c0
kuna84 <kuna84@users.noreply.github.com>
2019-01-04 23:09:08 +0100

    rozdział_2: ćwicznie 4

---
+++ rozdzial_2/eggs.py
@@ -0,0 +1,15 @@
+pharse = "Podaj jajko!"
+plist = list(pharse)
+print(pharse)
+print(plist)
+unwanted_letters = ['P', 'a', 'j', 'k', 'o', '!']
+
+for letter in plist:
+    print(letter)
+    if letter in unwanted_letters:
+        plist
+        
+
+#new_pharse = ''.join(plist)
+#print(plist)
+#print(new_pharse)

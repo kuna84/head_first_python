e916e6570631118532052ab07c8f8f55a4cb73f9
kuna84 <kuna84@users.noreply.github.com>
2019-01-04 22:05:52 +0100

    rozdział_2: przykład 1

--- vovels.py
+++ vovels.py
@@ -1,10 +1,6 @@
 vovels = ['a', 'e', 'i', 'o', 'u']
 word = "miliard"
-found = []
 
 for letter in word:
     if letter in vovels:
-        if letter not in found:
-            found.append(letter)
-for vovel in found:
-    print(vovel)
+        print(letter)

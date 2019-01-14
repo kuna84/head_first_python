2b3eb7455135b98ea898f0c1948254b08c384acd
Radek Kaczmarek <radoslaw.kaczmarek@mobica.com>
2019-01-14 11:58:06 +0100

    skrypt i zadania z rozdziału 4, funkcje search4vowels i saerch4leters. mymodules przygotowane do stworzenia modułu

---
+++ mymodules/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vsearch',
+    version='1.0',
+    description='Narzędzia do wyszukiwania dla książki Python. Rusz głową!',
+    author='Python, Rusz głową! Wydnie II',
+    author_email='qwertyu@qwer.com',
+    url='headfirstlabs.com',
+    py_modules=['vsearch'],
+)
---
+++ mymodules/vsearch.py
@@ -0,0 +1,8 @@
+def search4vowels(word:str) -> set:
+    """Wyświetla samogłoski znalezione w słowie podanym przez użytkownika"""
+    vovels = set('aeiou')
+    return vovels.intersection(set(word))
+
+def search4letters(phrase:str, letters:str='aeiouy') -> set:
+    """Zwraca zbiór liter podanych w zmiennej letters znalezionych w zmiennej pharse"""
+    return set(letters).intersection(set(phrase))

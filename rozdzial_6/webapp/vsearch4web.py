6c937a39a1186426857cfdc78476517cd117a09a
Radek Kaczmarek <radoslaw.kaczmarek@mobica.com>
2019-04-03 09:26:36 +0200

    rozdzial_5, rozdial_6

---
+++ rozdzial_6/webapp/vsearch4web.py
@@ -0,0 +1,34 @@
+
+from flask import Flask, render_template, request
+from vsearch import search4letters
+
+app = Flask(__name__)
+
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsaerch.log', 'a') as log:
+        print(req, res, file=)
+
+@app.route('/search4', methods=['POST'])
+def do_search() -> 'html':
+   phrase = request.form['phrase']
+   letters = request.form['letters']
+   title = 'Oto Twoje wyniki:'
+   results = str(search4letters(phrase, letters))
+   return render_template('results.html',
+                          the_title=title,
+                          the_phrase=phrase,
+                          the_letters=letters,
+                          the_results=results,)
+
+
+@app.route('/')
+@app.route('/entry')
+def entry_page() -> 'html':
+   return render_template('entry.html',
+                          the_title='Witamy na stronie internetowej search4letters!')
+
+
+if __name__ == '__main__':
+   app.run(debug=True)
+
+
